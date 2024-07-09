afd78a3d90170042b0873f2cce5296b5c34b02b0
PHJUNITY <45454119+PHJUNITY@users.noreply.github.com>
2024-07-09 20:13:28 +0900

    :white_check_mark: 테스트 코드 최종 작성(페이지 네이션 수정후 테스트 코드 수정 필요)

--- src/test/kotlin/com/teamsparta/withdog/domain/api/PostServiceTest.kt
+++ src/test/kotlin/com/teamsparta/withdog/domain/api/PostServiceTest.kt
@@ -2,6 +2,8 @@ package com.teamsparta.withdog.domain.api
 
 
 import com.teamsparta.withdog.domain.post.dto.PostRequest
+import com.teamsparta.withdog.domain.post.dto.PostResponse
+import com.teamsparta.withdog.domain.post.model.Post
 import com.teamsparta.withdog.domain.post.repository.PostRepository
 import com.teamsparta.withdog.domain.post.service.PostService
 import com.teamsparta.withdog.domain.user.dto.UserSignUpRequest
@@ -66,6 +68,7 @@ class PostServiceTest{
     @Test
     fun `캐시를 적용한 검색 api가 처음 조회했을때 캐시에 등록이 되는지 테스트 하는 함수`(){
 
+
         val postResponse =postService.createPost(
             image = null,
             userId = 1,
@@ -84,6 +87,76 @@ class PostServiceTest{
         postService.getPostById(postId)
 
         cacheManager.getCache("postCache")!!.get(postId) shouldNotBe null
+
+        val cachedPost =cacheManager.getCache("postCache")!!.get(postId)!!.get() as PostResponse
+        cachedPost.id shouldBe postId
+        cachedPost.title shouldBe "캐시 테스트코드용 제목"
+
+        postService.deletePost(userId = 1, postId = postId)
+
+        cacheManager.getCache("postCache")!!.get(postId) shouldBe null
+
     }
+
+    @Transactional
+    @Test
+    fun `새로운 포스트를 만들면 해당 포스트의 견종에 대한 검색 리스트의 캐시가 삭제되는지 테스트하는 함수`(){
+
+        postService.getPostByKeyword(
+            page= 0,
+            size= 10,
+            sortBy= "createdAt",
+            direction= "desc",
+            keyword= "해리포터"
+        )
+        cacheManager.getCache("keywordPostCache")!!.get("해리포터") shouldNotBe null
+
+        //조회 했으니 캐시가 있어야 하며 create를 하면 캐시가 삭제되어야함
+
+        postService.createPost(
+            image = null,
+            userId = 1,
+            postRequest = PostRequest(
+                title = "캐시 테스트코드용 제목",
+                content = "캐시 테스트 코드용 내용",
+                breedName = "해리포터"
+            )
+        )
+
+        cacheManager.getCache("keywordPostCache")!!.get("해리포터") shouldBe null
+
+    }
+
+    @Transactional
+    @Test
+    fun `캐시를 적용하지 않은 키워드검색api를 이용하면 실제로 캐시가 생성되지 않는지 테스트하는 함수`(){
+
+        postService.createPost(
+            image = null,
+            userId = 1,
+            postRequest = PostRequest(
+                title = "캐시 테스트코드용 제목",
+                content = "캐시 테스트 코드용 내용",
+                breedName = "스타워즈"
+            )
+        )
+        cacheManager.getCache("keywordPostCache")!!.get("스타워즈") shouldBe null
+
+        postService.getPostByKeywordNoCache(
+            page= 0,
+            size= 10,
+            sortBy= "createdAt",
+            direction= "desc",
+            keyword= "스타워즈"
+        )
+
+        cacheManager.getCache("keywordPostCache")!!.get("스타워즈") shouldBe null
+
+
+
+    }
+
+
+
 
 }
